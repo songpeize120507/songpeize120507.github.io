ee06d47e2afd39f8e9042119dfa74408e5462a33
songpeize120507 <songpeize050712@outlook.com>
2023-12-30 09:43:02 +0800

    Site updated: 2023-12-30 09:43:02

--- js/runtime.js
+++ js/runtime.js
@@ -1,1 +1,30 @@
-var now = new Date; function createtime() { now.setTime(now.getTime() + 1e3); var e = new Date("11/12/2023 00:00:00"), t = Math.trunc(234e8 + (now - e) / 1e3 * 17), a = (t / 1496e5).toFixed(6), o = new Date("11/12/2023 00:00:00"), n = (now - o) / 1e3 / 60 / 60 / 24, r = Math.floor(n), i = (now - o) / 1e3 / 60 / 60 - 24 * r, s = Math.floor(i); 1 == String(s).length && (s = "0" + s); var d = (now - o) / 1e3 / 60 - 1440 * r - 60 * s, l = Math.floor(d); 1 == String(l).length && (l = "0" + l); var g = (now - o) / 1e3 - 86400 * r - 3600 * s - 60 * l, b = Math.round(g); 1 == String(b).length && (b = "0" + b); let c = ""; <div style="font-size:13px;font-weight:bold">本站已运行 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>` : ` < img class='boardsign' src = 'https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title = '下班了就该开开心心地玩耍~' > <br> <div style="font-size:13px;font-weight:bold">本站已运行 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`, document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = c) } setInterval((() => {createtime()}), 1e3);
+setInterval(() => {
+    let create_time = Math.round(new Date('2023-12-02 00:00:00').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+
+    var nol = function (h) {
+        return h > 9 ? h : '0' + h;
+    }
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[2] = nol(parseInt(second / 3600));
+        second %= 3600;
+    };
+    if (second >= 60) {
+        time[3] = nol(parseInt(second / 60));
+        second %= 60;
+    }
+    if (second > 0) {
+        time[4] = nol(second);
+    }
+    document.getElementById("workboard").innerHTML = currentTimeHtml;
+}, 1000);
